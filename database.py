c0882e02cef5f77fac206e8015dc9a787fcf04d6
Coulibaly Hamidou Baba <Coulibalyhamidoubaba@gmail.com>
2026-06-13 14:01:44 +0000

    Fix port DB par defaut (5432)

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ DB_CONFIG = {
     "user": os.getenv("DB_USER", "cabinet_user"),
     "password": os.getenv("DB_PASSWORD", "BabaMouneissa2026!"),
     "host": os.getenv("DB_HOST", "localhost"),
-    "port": os.getenv("DB_PORT", "5433"),
+    "port": os.getenv("DB_PORT", "5432"),
 }
 
 def get_db():
